404af384a97ad58119f958cf94144c8be5fd5a5d
FernanDezlo93 <fernandofag@correo.ugr.es>
2023-07-10 11:07:15 +0200

    Composer upgrade/install and Docker-Compose

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cf452bc2f1bd92e3eb7f42a2aae73395328548ef',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cf452bc2f1bd92e3eb7f42a2aae73395328548ef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tourcms/tourcms-php' => array(
+            'pretty_version' => 'v4.1.0',
+            'version' => '4.1.0.0',
+            'reference' => '9e4e68df2bb3b5b2ab738d8d21c5bd358256ce52',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tourcms/tourcms-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
